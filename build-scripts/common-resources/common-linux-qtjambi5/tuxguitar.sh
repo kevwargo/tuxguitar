44bfeec3457f7757690a4d1b798f9a8bbc65fc2a
Jaraš Kevwargo <jarasz.kevwargo@gmail.com>
2022-09-04 17:01:56 +0200

    Use the absolute paths in CLASSPATH and LD_LIBRARY_PATH

--- build-scripts/common-resources/common-linux-qtjambi5/tuxguitar.sh
+++ build-scripts/common-resources/common-linux-qtjambi5/tuxguitar.sh
@@ -1,41 +1,36 @@
 #!/bin/sh
 ##SCRIPT DIR
-DIR_NAME=`dirname "$0"`
-DIR_NAME=`cd "$DIR_NAME"; pwd`
-cd "${DIR_NAME}"
-##JAVA
-JAVA_BUNDLED="./jre/bin/java"
-if [ -f "${JAVA_BUNDLED}" ]; then
-	JAVA="${JAVA_BUNDLED}"
-fi
+DIR_NAME=`dirname "$(realpath "$0")"`
+
 if [ -z ${JAVA} ]; then
 	[ -z ${JAVA_HOME} ] && JAVA_HOME="/usr"
 	[ ! -f "${JAVA}" ] && JAVA="${JAVA_HOME}/bin/java"
 	[ ! -f "${JAVA}" ] && JAVA="java"
 fi
 ##LIBRARY_PATH
-LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:lib/
+LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:"${DIR_NAME}/lib/"
 LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/lib
 LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/lib
 LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/lib/jni
 LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib
 ##CLASSPATH
-CLASSPATH=${CLASSPATH}:./lib/tuxguitar.jar
-CLASSPATH=${CLASSPATH}:./lib/tuxguitar-ui-toolkit.jar
-CLASSPATH=${CLASSPATH}:./lib/tuxguitar-ui-toolkit-qt5.jar
-CLASSPATH=${CLASSPATH}:./lib/tuxguitar-lib.jar
-CLASSPATH=${CLASSPATH}:./lib/tuxguitar-editor-utils.jar
-CLASSPATH=${CLASSPATH}:./lib/tuxguitar-gm-utils.jar
-CLASSPATH=${CLASSPATH}:./lib/tuxguitar-awt-graphics.jar
-CLASSPATH=${CLASSPATH}:./lib/qtjambi.jar
-CLASSPATH=${CLASSPATH}:./lib/qtjambi-native.jar
-CLASSPATH=${CLASSPATH}:./lib/gervill.jar
-CLASSPATH=${CLASSPATH}:./lib/itext-pdf.jar
-CLASSPATH=${CLASSPATH}:./lib/itext-xmlworker.jar
-CLASSPATH=${CLASSPATH}:./lib/commons-compress.jar
-CLASSPATH=${CLASSPATH}:./lib/icedtea-sound.jar
-CLASSPATH=${CLASSPATH}:./share/
-CLASSPATH=${CLASSPATH}:./dist/
+CLASSPATH=${CLASSPATH}:"${DIR_NAME}/lib/tuxguitar.jar"
+CLASSPATH=${CLASSPATH}:"${DIR_NAME}/lib/tuxguitar-ui-toolkit.jar"
+CLASSPATH=${CLASSPATH}:"${DIR_NAME}/lib/tuxguitar-ui-toolkit-qt5.jar"
+CLASSPATH=${CLASSPATH}:"${DIR_NAME}/lib/tuxguitar-lib.jar"
+CLASSPATH=${CLASSPATH}:"${DIR_NAME}/lib/tuxguitar-editor-utils.jar"
+CLASSPATH=${CLASSPATH}:"${DIR_NAME}/lib/tuxguitar-gm-utils.jar"
+CLASSPATH=${CLASSPATH}:"${DIR_NAME}/lib/tuxguitar-awt-graphics.jar"
+CLASSPATH=${CLASSPATH}:"${DIR_NAME}/lib/qtjambi.jar"
+CLASSPATH=${CLASSPATH}:"${DIR_NAME}/lib/qtjambi-native.jar"
+CLASSPATH=${CLASSPATH}:"${DIR_NAME}/lib/gervill.jar"
+CLASSPATH=${CLASSPATH}:"${DIR_NAME}/lib/itext-pdf.jar"
+CLASSPATH=${CLASSPATH}:"${DIR_NAME}/lib/itext-xmlworker.jar"
+CLASSPATH=${CLASSPATH}:"${DIR_NAME}/lib/commons-compress.jar"
+CLASSPATH=${CLASSPATH}:"${DIR_NAME}/lib/icedtea-sound.jar"
+CLASSPATH=${CLASSPATH}:"${DIR_NAME}/lib/jlp.jar"
+CLASSPATH=${CLASSPATH}:"${DIR_NAME}/share/"
+CLASSPATH=${CLASSPATH}:"${DIR_NAME}/dist/"
 ##MAINCLASS
 MAINCLASS=org.herac.tuxguitar.app.TGMainSingleton
 ##JVM ARGUMENTS
